154ea765bc5309dee89d9dd159259a9d07ace8f4
Suganya Sriramulu <suganya.sriramulu@avanade.com>
2026-04-02 14:29:50 +0800

    Added set -e

--- start_all.sh
+++ start_all.sh
@@ -1,11 +1,13 @@
-
-
 #!/bin/bash
+set -e
+
 echo "=============================================="
 echo "[BOOT] Starting DMW Validator Web UI on port 8085..."
-python3 -m uvicorn ui_app:app \
+
+exec python3 -m uvicorn ui_app:app \
   --host 0.0.0.0 \
   --port 8085
+``
 echo "----------------------------------------------"
 echo "[READY] ✅ Services running"
 echo "[INFO] Web UI : http://localhost:8085"
